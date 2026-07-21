4e173b374185586ee9d44a8d04de638a1c105de4
ferlyT <ferly.trianderi@gmail.com>
2026-07-21 08:39:51 +0700

    refactor: kelompokkan pages shipments ke features/shipments (SSOT lain tidak berubah)

---
+++ frontend/src/features/shipments/index.ts
@@ -0,0 +1,3 @@
+export { default as ShipmentsPage } from './pages/ShipmentsPage';
+export { default as ShipmentsListPage } from './pages/ShipmentsListPage';
+export { default as ShipmentsDashboardPage } from './pages/ShipmentsDashboardPage';
